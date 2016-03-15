f11486daea8d76c65f3d79d20e3b1f9c5555ff1b
sabrinamusatian <sabrina@MacBook-Pro-Sabrina.local>
2016-03-15 22:03:42 +0300

    balance

---
+++ task1/balance.scala
@@ -0,0 +1,25 @@
+
+
+object balance {
+  def balance(chars: List[Char]): Boolean = {
+		def Iter(char: List[Char], num: Int): Boolean = {
+			if (char.isEmpty){
+				num == 0
+			}
+			else if (char.head == '('){
+				Iter(char.tail,num + 1)
+			}
+			else if (char.head == ')'){
+				if (num > 0)
+					Iter(char.tail,num - 1)
+				else
+					false
+			}
+			else{
+				Iter(char.tail, num)
+			}
+		}
+		Iter(chars, 0)
+	}                                         //> balance: (chars: List[Char])Boolean
+	
+}
