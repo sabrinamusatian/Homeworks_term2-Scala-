4c6bf9c7a375327b3ded6c13d51fff905c569e27
sabrinamusatian <sabrina.musatian@yandex.ru>
2016-03-15 22:11:47 +0300

    Update balance.scala

--- task1/balance.scala
+++ task1/balance.scala
@@ -1,23 +1,22 @@
 object balance {
-  def balance(chars: List[Char]): Boolean = {
-		def Iter(char: List[Char], num: Int): Boolean = {
-			if (char.isEmpty){
-				num == 0
-			}
-			else if (char.head == '('){
-				Iter(char.tail,num + 1)
-			}
-			else if (char.head == ')'){
-				if (num > 0)
-					Iter(char.tail,num - 1)
-				else
-					false
-			}
-			else{
-				Iter(char.tail, num)
-			}
+ def balance(chars: List[Char]): Boolean = {
+	def Iter(char: List[Char], num: Int): Boolean = {
+		if (char.isEmpty){
+		num == 0
 		}
-		Iter(chars, 0)
-	}                                         //> balance: (chars: List[Char])Boolean
-	
+		else if (char.head == '('){
+			Iter(char.tail,num + 1)
+		}
+		else if (char.head == ')'){
+			if (num > 0)
+				Iter(char.tail,num - 1)
+			else
+				false
+		}
+		else{
+			Iter(char.tail, num)
+		}
+	}
+	Iter(chars, 0)
+}                                         //> balance: (chars: List[Char])Boolean
 }
