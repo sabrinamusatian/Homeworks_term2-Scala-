9980406ab91c00d58e24e4f26ea49f026dc9db13
sabrinamusatian <sabrina@MacBook-Pro-Sabrina.local>
2016-04-13 00:02:38 +0300

    commit change

--- task3_sets/FunSets.scala
+++ task3_sets/FunSets.scala
@@ -6,43 +6,48 @@ object FunSets {
   * Indicates whether a set contains a given element.
   */
   def contains(s: Set, elem: Int): Boolean = s(elem)
+                                        
                                                 
   /**
   * Returns the set of the one given element.
   */
   def singletonSet(elem: Int): Set = x => (x == elem)
+                                                  
                                                  
 
   /**
   * Returns the union of the two given sets,
   * the sets of all elements that are in either `s` or `t`.
   */
-  def union(s: Set, t: Set): Set = x => t(x) || s(x)                        
+  def union(s: Set, t: Set): Set = x => contains(t, x) || contains(s, x)
+                                          
   
   /**
   * Returns the intersection of the two given sets,
   * the set of all elements that are both in `s` and `t`.
   */
-  def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
+  def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t, x)
+                  
   
   
   /**
   * Returns the difference of the two given sets,
   * the set of all elements of `s` that are not in `t`.
   */
-  def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
-                                                  
+  def diff(s: Set, t: Set):  Set = x => contains(s, x) && !contains(t, x)
+                                               
  
   /**
   * Returns the subset of `s` for which `p` holds.
   */
-  def filter(s: Set, p: Int => Boolean): Set = intersect(s, p)
+  def filter(s: Set, p: Int => Boolean): Set = x => contains(s, x) && p(x)
+                                                 
                                                  
 
   /**
   * The bounds for `forall` and `exists` are +/- 1000.
   */
-  val bound = 1000
+  val bound = 1000                              
   
   /**
   * Displays the contents of a set
@@ -50,7 +55,7 @@ object FunSets {
   def toString(s: Set): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
     xs.mkString("{", ",", "}")
-  }
+  }                                             
   
   /**
   * Returns whether all bounded integers within `s` satisfy `p`.
@@ -58,17 +63,18 @@ object FunSets {
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (a > bound) true
-      else if (s(a) && !p(a)) false
+      else if (contains(s, a) && !p(a)) false
       else iter(a + 1)
     }
     iter(-bound)
-  }
+  }                                            
   
   /**
   * Returns whether there exists a bounded integer within `s`
   * that satisfies `p`.
   */
   def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
+                                                 
   
   /**
   * Returns a set transformed by applying `f` to each element of `s`.
@@ -82,5 +88,5 @@ object FunSets {
       else iter(a + 1)
     }
     iter(-bound)
-  }
+  }                                               
 }
